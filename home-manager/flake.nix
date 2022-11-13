8768dbb8e888bdc73094c69d445782114ffa7160
Nathan Fowler <fowl0074@gmail.com>
2022-11-13 23:47:23 +1030

    Started home-manager config

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -15,7 +15,7 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations.jdoe = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations.nfowler = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "nathan";
-  home.homeDirectory = "/home/nathan";
+  home.username = "nfowler";
+  home.homeDirectory = "/home/nfowler";
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
@@ -18,13 +18,66 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   home.packages = [
+    #Tools
+    pkgs.age
+    pkgs.antibody
     pkgs.bat
-    pkgs.fzf
-    pkgs.jq
-    pkgs.ripgrep
-    pkgs.fd
+    pkgs.delta
     pkgs.exa
-    pkgs.zoxide
+    pkgs.fd
+    pkgs.fzf
+    pkgs.gnumake
     pkgs.htop
+    pkgs.jq
+    pkgs.k9s
+    pkgs.neovim
+    pkgs.ripgrep
+    pkgs.starship
+    pkgs.tmux
+    pkgs.zoxide
+    pkgs.zsh
+    # Languages/Runtimes
+    pkgs.clang
+    pkgs.deno
+    pkgs.go
+    pkgs.nodejs-18_x
+    pkgs.rustup
+    pkgs.terraform
   ];
+
+  programs.zsh = {
+    enable = true;
+    # initExtra = ''
+    #   # . ~/workspace/dotfiles/zsh/.zshrc
+    # '';
+    # envExtra = ''
+    #   # . ~/workspace/dotfiles/zsh/.zshenv
+    # '';
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      {
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-show-battery false
+          set -g @dracula-show-powerline true
+          set -g @dracula-refresh-rate 10
+        '';
+      }
+    ];
+  };
 }
