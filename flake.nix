abaf8d50f9fe867938bb703697b0446d279c0df1
Nathan Fowler <fowl0074@gmail.com>
2023-08-03 20:39:46 +0930

    Moved update deps to flake shell to allow tracking nixpkgs/home-manager

--- flake.nix
+++ flake.nix
@@ -8,37 +8,50 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations.personal = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = { nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        homeConfigurations.personal = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          ./personal.nix
-        ];
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [
+            ./personal.nix
+          ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-      homeConfigurations.work = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+        homeConfigurations.work = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          ./work.nix
-        ];
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [
+            ./work.nix
+          ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bash
+            pkgs.niv
+            pkgs.nix
+            pkgs.jq
+            pkgs.home-manager
+          ];
+        };
+      }
+    );
 }
--- update-deps.sh
+++ update-deps.sh
@@ -1,1 +1,4 @@
-nix-shell --run "niv update"
+nix develop --command niv "update"
+nix develop --command niv update nixpkgs-unstable -r $(nix flake metadata nixpkgs --json | jq -r '.revision')
+nix develop --command niv update nixpkgs -r $(nix flake metadata nixpkgs --json | jq -r '.revision')
+nix develop --command niv update home-manager -r $(nix flake metadata home-manager --json | jq -r '.revision')
