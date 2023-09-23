43daf3d88eb53fcdff4dc40d1d1be49427d67e82
Nathan Fowler <fowl0074@gmail.com>
2023-09-23 23:55:09 +0930

    merged

--- modules/helix/helix.nix
+++ modules/helix/helix.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
-
+let 
+  sources = (import ../../nix/sources.nix);
+in
 {
+  xdg.configFile."helix/runtime/queries/cloudflare/highlights.scm".source = sources.tree-sitter-cloudflare + "/queries/highlights.scm";
   xdg.configFile."helix/config.toml".source = ./config.toml;
   xdg.configFile."helix/languages.toml".source = ./languages.toml;
 }
